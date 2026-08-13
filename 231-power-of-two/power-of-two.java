5c67a1f4ad2a991a02a79eaf984cb876bcc2715f
Shanmukha Ganesh <2400031615@kluniversity.in>
2026-08-13 11:37:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (74.21%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n>0){
+            if((n&(n-1)) ==0 ){
+                return true;
+            }
+        }
+        return false;
+    }
+}
